edc20800b57de93c1d1c56ecd44096d2a1025b39
Tom <mr@tomclement.nl>
2024-08-10 12:12:24 +0200

    Added Boekenwuurm's graph data and force

---
+++ src/views/Root.tsx
@@ -0,0 +1,156 @@
+import { FullScreenControl, SigmaContainer, ZoomControl } from "@react-sigma/core";
+import { createNodeImageProgram } from "@sigma/node-image";
+import { DirectedGraph } from "graphology";
+import { constant, keyBy, mapValues, omit } from "lodash";
+import { FC, useEffect, useMemo, useState } from "react";
+import { BiBookContent, BiRadioCircleMarked } from "react-icons/bi";
+import { BsArrowsFullscreen, BsFullscreenExit, BsZoomIn, BsZoomOut } from "react-icons/bs";
+import { GrClose } from "react-icons/gr";
+import { Settings } from "sigma/settings";
+
+import { drawHover, drawLabel } from "../canvas-utils";
+import { Dataset, FiltersState } from "../types";
+import ClustersPanel from "./ClustersPanel";
+import DescriptionPanel from "./DescriptionPanel";
+import GraphDataController from "./GraphDataController";
+import GraphEventsController from "./GraphEventsController";
+import GraphSettingsController from "./GraphSettingsController";
+import GraphTitle from "./GraphTitle";
+import SearchField from "./SearchField";
+import TagsPanel from "./TagsPanel";
+
+const Root: FC = () => {
+  const [showContents, setShowContents] = useState(false);
+  const [dataReady, setDataReady] = useState(false);
+  const [dataset, setDataset] = useState<Dataset | null>(null);
+  const [filtersState, setFiltersState] = useState<FiltersState>({
+    clusters: {},
+    tags: {},
+  });
+  const [hoveredNode, setHoveredNode] = useState<string | null>(null);
+  const sigmaSettings: Partial<Settings> = useMemo(
+    () => ({
+      nodeProgramClasses: {
+        image: createNodeImageProgram({
+          size: { mode: "force", value: 256 },
+        }),
+      },
+      defaultDrawNodeLabel: drawLabel,
+      defaultDrawNodeHover: drawHover,
+      defaultNodeType: "image",
+      defaultEdgeType: "arrow",
+      labelDensity: 0.07,
+      labelGridCellSize: 60,
+      labelRenderedSizeThreshold: 15,
+      labelFont: "Lato, sans-serif",
+      zIndex: true,
+    }),
+    [],
+  );
+
+  // Load data on mount:
+  useEffect(() => {
+    fetch(`./dataset.json`)
+      .then((res) => res.json())
+      .then((dataset: Dataset) => {
+        setDataset(dataset);
+        setFiltersState({
+          clusters: mapValues(keyBy(dataset.clusters, "key"), constant(true)),
+          tags: mapValues(keyBy(dataset.tags, "key"), constant(true)),
+        });
+        requestAnimationFrame(() => setDataReady(true));
+      });
+  }, []);
+
+  if (!dataset) return null;
+
+  return (
+    <div id="app-root" className={showContents ? "show-contents" : ""}>
+      <SigmaContainer graph={DirectedGraph} settings={sigmaSettings} className="react-sigma">
+        <GraphSettingsController hoveredNode={hoveredNode} />
+        <GraphEventsController setHoveredNode={setHoveredNode} />
+        <GraphDataController dataset={dataset} filters={filtersState} />
+
+        {dataReady && (
+          <>
+            <div className="controls">
+              <div className="react-sigma-control ico">
+                <button
+                  type="button"
+                  className="show-contents"
+                  onClick={() => setShowContents(true)}
+                  title="Show caption and description"
+                >
+                  <BiBookContent />
+                </button>
+              </div>
+              <FullScreenControl className="ico">
+                <BsArrowsFullscreen />
+                <BsFullscreenExit />
+              </FullScreenControl>
+
+              <ZoomControl className="ico">
+                <BsZoomIn />
+                <BsZoomOut />
+                <BiRadioCircleMarked />
+              </ZoomControl>
+            </div>
+            <div className="contents">
+              <div className="ico">
+                <button
+                  type="button"
+                  className="ico hide-contents"
+                  onClick={() => setShowContents(false)}
+                  title="Show caption and description"
+                >
+                  <GrClose />
+                </button>
+              </div>
+              <GraphTitle filters={filtersState} />
+              <div className="panels">
+                <SearchField filters={filtersState} />
+                <DescriptionPanel />
+                <ClustersPanel
+                  clusters={dataset.clusters}
+                  filters={filtersState}
+                  setClusters={(clusters) =>
+                    setFiltersState((filters) => ({
+                      ...filters,
+                      clusters,
+                    }))
+                  }
+                  toggleCluster={(cluster) => {
+                    setFiltersState((filters) => ({
+                      ...filters,
+                      clusters: filters.clusters[cluster]
+                        ? omit(filters.clusters, cluster)
+                        : { ...filters.clusters, [cluster]: true },
+                    }));
+                  }}
+                />
+                <TagsPanel
+                  tags={dataset.tags}
+                  filters={filtersState}
+                  setTags={(tags) =>
+                    setFiltersState((filters) => ({
+                      ...filters,
+                      tags,
+                    }))
+                  }
+                  toggleTag={(tag) => {
+                    setFiltersState((filters) => ({
+                      ...filters,
+                      tags: filters.tags[tag] ? omit(filters.tags, tag) : { ...filters.tags, [tag]: true },
+                    }));
+                  }}
+                />
+              </div>
+            </div>
+          </>
+        )}
+      </SigmaContainer>
+    </div>
+  );
+};
+
+export default Root;
